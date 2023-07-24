f0c85b2546720e0a8c2382fd208a9d8a46b5db94
edenacyg <137974995+denacayago@users.noreply.github.com>
2023-07-24 23:31:25 +0800

    timer 02

--- src/App.jsx
+++ src/App.jsx
@@ -5,96 +5,102 @@ import Card from "./components/Cards";
 import shuffle from "./utitilies/shuffle";
 import Header from "./components/Header";
 import useAppBadge from "./hooks/useAppBadge";
+import Timer from "./components/Timer";
 
 function App() {
-  const [cards, setCards] = useState(shuffle); //Shuffle Cards
-  const [pickOne, setPickOne] = useState(null); //First Selection
-  const [pickTwo, setPickTwo] = useState(null); //Second Selection
-  const [disabled, setDisabled] = useState(false); //Selection Delay
-  const [wins, setWins] = useState(0); //Number of Wins
-  const [setBadge, clearBadge] = useAppBadge(); //Handles App Badge
+	const [cards, setCards] = useState(shuffle); //Shuffle Cards
+	const [pickOne, setPickOne] = useState(null); //First Selection
+	const [pickTwo, setPickTwo] = useState(null); //Second Selection
+	const [disabled, setDisabled] = useState(false); //Selection Delay
+	const [wins, setWins] = useState(0); //Number of Wins
+	const [setBadge, clearBadge] = useAppBadge(); //Handles App Badge
+	const [isStart, setStart] = useState(false); //Start
 
-  //Handle Card Selections
-  const handleClick = (card) => {
-    if (!disabled) {
-      pickOne ? setPickTwo(card) : setPickOne(card);
-    }
-  };
+	//Handle Card Selections
+	const handleClick = (card) => {
+		setStart(true);
 
-  const handleTurn = () => {
-    setPickOne(null);
-    setPickTwo(null);
-    setDisabled(false);
-  };
+		if (!disabled) {
+			pickOne ? setPickTwo(card): setPickOne(card);
+		}
+	};
 
-  const handleNewGame = () => {
-    setWins(0);
-    clearBadge();
-    handleTurn();
-    setCards(shuffle);
-  };
+	const handleTurn = () => {
+		setPickOne(null);
+		setPickTwo(null);
+		setDisabled(false);
+	};
 
-  useEffect(() => {
-    let pickTimer;
+	const handleNewGame = () => {
+		setWins(0);
+		clearBadge();
+		handleTurn();
+		setCards(shuffle);
+	};
 
-    // Match Click Handler Logic
-    if (pickOne && pickTwo) {
-      if (pickOne.image === pickTwo.image) {
-        setCards((prevCards) => {
-          return prevCards.map((card) => {
-            if (card.image === pickOne.image) {
-              return { ...card, matched: true };
-            } else {
-              return card;
-            }
-          });
-        });
-        handleTurn();
-      } else {
-        setDisabled(true);
-        pickTimer = setTimeout(() => {
-          handleTurn();
-        }, 500);
-      }
-    }
+	useEffect(() => {
+		let pickTimer;
 
-    return () => {
-      clearTimeout(pickTimer);
-    };
-  }, [cards, pickOne, pickTwo, setBadge, wins]);
+		// Match Click Handler Logic
+		if (pickOne && pickTwo) {
+			if (pickOne.image === pickTwo.image) {
+				setCards((prevCards) => {
+					return prevCards.map((card) => {
+						if (card.image === pickOne.image) {
+							return { ...card, matched: true };
+						} else {
+							return card;
+						}
+					});
+				});
+				handleTurn();
+			} else {
+				setDisabled(true);
+				pickTimer = setTimeout(() => {
+					handleTurn();
+				}, 500);
+			}
+		}
 
-  useEffect(() => {
-    const checkWin = cards.filter((card) => !card.matched);
-    if (cards.length && checkWin.length < 1) {
-      console.log("Goodjob Hehe");
-      setWins(wins + 1);
-      setBadge();
-      handleTurn();
-      setCards(shuffle);
-    }
-  }, [cards, setBadge, wins]);
+		return () => {
+			clearTimeout(pickTimer);
+		};
+	}, [cards, pickOne, pickTwo, setBadge, wins]);
 
-  return (
-    <>
-      <Header handleNewGame={handleNewGame} wins={wins} />
+	useEffect(() => {
+		const checkWin = cards.filter((card) => !card.matched);
+		if (cards.length && checkWin.length < 1) {
+			console.log("Goodjob Hehe");
+			setWins(wins + 1);
+			setBadge();
+			handleTurn();
+			setCards(shuffle);
+			setStart(false);
+		}
+	}, [cards, setBadge, wins]);
 
-      <div className="grid">
-        {cards.map((card) => {
-          const { image, matched } = card;
+	return (
+		<>
+			<Timer isStart={isStart} wins={wins}/>
+			<Header handleNewGame={handleNewGame} wins={wins} />
 
-          return (
-            <Card
-              key={image.id}
-              card={card}
-              image={image}
-              selected={card === pickOne || card === pickTwo || matched}
-              onClick={() => handleClick(card)}
-            />
-          );
-        })}
-      </div>
-    </>
-  );
+			<div className="grid">
+				{cards.map((card) => {
+					const { image, matched } = card;
+
+					return (
+						<Card
+							key={image.id}
+							card={card}
+							image={image}
+							selected={card === pickOne || card === pickTwo || matched}
+							onClick={() => handleClick(card)}
+						/>
+					);
+				})}
+			</div>
+		</>
+	);
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 
 const Header = ({ handleNewGame, wins }) => {
-<<<<<<< HEAD
 	// Update page title with win count
 	useEffect(() => (document.title = `${wins} wins`), [wins]);
 
@@ -18,18 +17,6 @@ const Header = ({ handleNewGame, wins }) => {
 		</>
 
 	);
-=======
-  // Update page title with win count
-  useEffect(() => (document.title = `${wins} wins`), [wins]);
-
-  return (
-    <header className="header">
-      <h3>Memory Game</h3>
-      <h4>{wins} wins</h4>
-      <button onClick={handleNewGame}>New Game</button>
-    </header>
-  );
->>>>>>> 70ddf018d597b48a1e0d0aa3555c8bdc7cfc6ca0
 };
 
 export default Header;
